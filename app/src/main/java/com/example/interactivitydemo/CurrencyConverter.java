e8f2c5be68d63fc0c13a3faa8cf504ee1e6e54d3
Ajay K Dixit <ajaykd@rediffmail.com>
2019-08-17 23:08:10 +0530

    Working AsyncTask + CurrencyConverter

--- app/src/main/java/com/example/interactivitydemo/CurrencyConverter.java
+++ app/src/main/java/com/example/interactivitydemo/CurrencyConverter.java
@@ -2,30 +2,44 @@ package com.example.interactivitydemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CurrencyConverter extends AppCompatActivity {
 
+    ProgressDialog pd;
+    TextView convertedCurrencyValueEditText;
+    float inrValue=0, usdValue=0;
+
     public void convertCurrency(View view) {
+
         EditText currencyToConvertEditText = findViewById(R.id.currencyEditText);
-        int userCurrency = Integer.parseInt(currencyToConvertEditText.getText().toString());
+        convertedCurrencyValueEditText = findViewById(R.id.currentUSDrateTextView);
 
-        String userConvertedCurrency = Integer.toString(userCurrency * 67);
-        Toast.makeText(this, "INR: " + userConvertedCurrency, Toast.LENGTH_LONG).show();
-
-        getCurrentRate();
+        new RetrieveAPITask().execute(currencyToConvertEditText.getText().toString());
     }
 
-    public float getCurrentRate() {
+    public float getCurrentRate() throws IOException {
         // Setting URL
         String url_str = "https://api.exchangerate-api.com/v4/latest/USD";
 
@@ -49,5 +63,64 @@ public class CurrencyConverter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency_converter);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    class RetrieveAPITask extends AsyncTask<String, Void, Void> {
+
+        private Exception exception;
+//        private EditText currencyToConvertEditText;
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(CurrencyConverter.this);
+            pd.setMessage("Please wait... getting current rates");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected Void doInBackground(String... strings) {
+            String url_str = "https://api.exchangerate-api.com/v4/latest/USD";
+            URL url = null;
+            try {
+                url = new URL(url_str);
+                Log.d("CurrencyConverter", String.valueOf(url));
+
+                HttpURLConnection request = (HttpURLConnection) url.openConnection();
+                request.connect();
+
+                Log.d("--> CurrencyCoverter", String.valueOf(request.toString()));
+                // Convert to JSON
+                JsonParser jp = new JsonParser();
+                Log.d("--> CurrencyCoverter", String.valueOf(request.getContent()));
+                JsonObject root = (JsonObject) jp.parse(new InputStreamReader((InputStream) request.getContent()));
+                JsonObject rates = root.getAsJsonObject("rates");
+                JsonElement INR = rates.get("INR");
+                Log.d("--> CurrencyCoverter", "doInBackground: " + INR);
+
+                inrValue = Float.valueOf(INR.getAsString());
+                usdValue = Float.valueOf(strings[0]);
+                Log.d("--> CurrencyCoverter", "1 USD = " + INR + " INR");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void v) {
+            float finalINRvalue = usdValue * inrValue;
+
+            Log.d("--> CurrencyCoverter", usdValue + " USD = " + finalINRvalue + " INR");
+            convertedCurrencyValueEditText.setText(String.format("%s USD = %s INR", Float.toString(usdValue), finalINRvalue));
+
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+        }
     }
 }
